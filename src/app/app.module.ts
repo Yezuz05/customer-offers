97ae19fd39c619cbfa44fcaade4c6841dff40a25
yezuz05 <temitopeyussuph@gmail.com>
2020-01-16 16:18:39 +0100

    feat: created offers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AvatarComponent } from './components/avatar/avatar.component';
+import { OffersComponent } from './pages/offers/offers.component';
 
 @NgModule({
-  declarations: [AppComponent, AvatarComponent],
+  declarations: [AppComponent, AvatarComponent, OffersComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
