d8c05872d4384e2558bc8721acfa425298d7508c
yezuz05 <temitopeyussuph@gmail.com>
2020-01-16 16:58:35 +0100

    feat: created subscriptions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AvatarComponent } from './components/avatar/avatar.component';
 import { OffersComponent } from './pages/offers/offers.component';
+import { SubscriptionsComponent } from './pages/subscriptions/subscriptions.component';
 
 @NgModule({
-  declarations: [AppComponent, AvatarComponent, OffersComponent],
+  declarations: [AppComponent, AvatarComponent, OffersComponent, SubscriptionsComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
